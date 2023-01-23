0f8b11a63b309854e8a986294c7665ef680a6855
juraj.rudy <jrudy134@gmail.com>
2023-01-23 14:42:21 +0100

    last commit

--- js/MainScript.js
+++ js/MainScript.js
@@ -1,17 +1,7 @@
-import * as CANNON from "cannon-es";
 class MainWorld{
 
     constructor() {
         this.Init();
-    }
-
-    SetBirdBasicState(object){
-        this.birdAxis = {
-            x: object.rotation.x,
-            y: object.rotation.y,
-            z: object.rotation.z
-        }
-
     }
 
     GetBird(){
@@ -32,44 +22,74 @@ class MainWorld{
 
     Init(){
         this.clock = new THREE.Clock();
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.shadowMap.enabled = true;
-        this.renderer.shadowMap.Type = THREE.VSM;
-        //this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-        document.body.appendChild(this.renderer.domElement);
-
-        window.addEventListener('resize', () => {
-            this.OnWindowResize();
-        }, false);
-
-        const fov = 60;
-        const aspect = 1920/1080;
-        const near = 1.0;
-        const far = 1000.0;
-        this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this.camera.position.set(0,2,-5);
-
-
-
+        this.AddRenderer();
 
         this.scene = new THREE.Scene();
-
         this.keyboard = new THREEx.KeyboardState();
+
+        this.camera1 = this.AddCamera(60,1920/1080,1.0,1000,{x:0,y:2,z:-5}, false);
+        this.camera2 = this.AddCamera(60, 1920/1080, 0.1, 500, {x:0,y:50,z:0}, false);
+        this.faceCam = this.AddCamera(60 , 1920/1080, 0.2, 500 , {x:0,y:-0.2,z:1.5}, true);
+
+        this.camera1Active = true;
+        this.camera2Active = false;
+        this.faceCamActive = false;
 
         this.AddObjects();
         this.AddLights();
+        this.AddCurve();
 
-        this.world = new CANNON.World({
-            gravity: new CANNON.Vec3(0,-9.81,0)
-        })
-        this.timeStep = 1/60;
+        this.controls = new THREE.OrbitControls( this.camera1, this.renderer.domElement );
 
-        this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+        //GUI
+        this.gui = new dat.GUI();
+        let cameras = ['TopCamera', 'BehindBirdCamera', 'FaceCamera'];
+        let cameraSwitch = {currentCamera: 'BehindBirdCamera'};
+        this.cameraSelect = this.gui.add(cameraSwitch, 'currentCamera', cameras).name('Cameras');
+        this.cameraSelect.onChange((value)=> this.UpdateCamera(value));
 
-        this.RAF();
     }
+
+    UpdateCamera(value){
+        if(value =='TopCamera'){
+            this.camera1Active = false;
+            this.camera2Active = true;
+            this.faceCamActive = false;
+        }
+        else if(value =='BehindBirdCamera'){
+            this.camera1Active = true;
+            this.camera2Active = false;
+            this.faceCamActive = false;
+        }
+        else if(value =='FaceCamera'){
+            this.camera1Active = false;
+            this.camera2Active = false;
+            this.faceCamActive = true;
+        }
+        else{
+            this.camera1Active = true;
+            this.camera2Active = false;
+            this.faceCamActive = false;
+        }
+    }
+
+    AddRenderer(){
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.Type = THREE.VSM;
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+    }
+    AddCamera(fov, aspect, near, far, position, rotateY){
+        let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        if(rotateY){
+            camera.rotateY(Math.PI)
+        }
+        camera.position.set(position.x, position.y, position.z);
+        return camera;
+    }
+
     AddObjects(){
 
         //SPHERE
@@ -119,28 +139,25 @@ class MainWorld{
 
             this.SetBird(gltf.scene);
 
-            const box = new THREE.BoxHelper( this.GetBird(), 0xffff00 );
-            this.GetBird().add(box);
-            this.GetBird().position.set(0,5,0);
+            // const box = new THREE.BoxHelper( this.GetBird(), 0xffff00 );
+            // this.GetBird().add(box);
+            this.GetBird().position.set(0,10,0);
             this.GetBird().scale.set(2,2,2);
             this.scene.add(this.GetBird());
             this.sideAngle = 0;
-            this.GetBird().add(this.camera);
+            this.GetBird().add(this.camera1);
+            this.GetBird().add(this.faceCam);
             this.mixer = new THREE.AnimationMixer(this.GetBird());
             const clips = gltf.animations;
             const clip = THREE.AnimationClip.findByName(clips, 'BirdFly');
             const action = this.mixer.clipAction(clip);
             action.play();
 
-            // const axesHelper = new THREE.AxesHelper( 5 );
-            // this.GetBird().add(axesHelper);
-
-            this.SetBirdBasicState(this.GetBird());
         });
     }
 
     LoadMuseum(path){
-        let counter;
+        let counter = 0;
         const loader = new THREE.GLTFLoader();
         loader.load(path, (gltf) => {
             gltf.scene.traverse(c => {
@@ -154,13 +171,7 @@ class MainWorld{
             this.GetMuseum().position.set(2,0,32);
             this.scene.add(this.GetMuseum());
 
-            const mShape = this.GetMuseum().shape();
-            this.museumBody = new CANNON.Body({
-                shape: mShape,
-                mass: 10,
-                // type: CANNON.Body.STATIC
-            })
-            this.world.addBody(this.museumBody)
+
         });
 
 
@@ -233,43 +244,124 @@ class MainWorld{
 
     }
 
-    OnWindowResize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(window.innerWidth / window.innerHeight);
-    }
-
-    RAF(){
-        requestAnimationFrame(()=> {
-            this.world.step(this.timeStep)
-
-            this.GetMuseum().position.copy(this.museumBody.position)
-            this.GetMuseum().quaternion.copy(this.museumBody.quaternion)
-
-            this.renderer.render(this.scene, this.camera);
-            this.RAF();
-        });
+    RequestAnimationFrame(){
+        requestAnimationFrame(()=> this.RequestAnimationFrame());
+        // this.renderer.render(this.scene, this.camera);
+        this.CameraSwitch();
         this.Update();
     }
 
+    CameraSwitch() {
+        if(this.camera1Active){
+            this.renderer.render(this.scene, this.camera1);
+        }else if (this.camera2Active){
+            this.renderer.render(this.scene, this.camera2);
+        }else if(this.faceCamActive){
+            this.renderer.render(this.scene, this.faceCam);
+        }
+    }
     Update(){
-        //const timeElapsedS = timeElapsed * 0.001;
         const delta = this.clock.getDelta();
-        this.mixer.update(delta);
+        try{
+            this.mixer.update(delta);
+        }catch (e){
+            ;
+        }
+
+        this.MoveOnCurve();
         this.KeyboardHandling(delta);
+
         this.controls.update();
+        this.camera2.lookAt(this.GetBird().position);
 
     }
 
+    AddCurve(){
+        this.curve = new THREE.CatmullRomCurve3( [
+            new THREE.Vector3( 60,20,15 ),
+
+            new THREE.Vector3( 0,10,17 ),
+
+            //INNER CIRCLE STATUES
+            new THREE.Vector3( 5,8,0 ),
+            new THREE.Vector3( 20,8,0 ),
+            new THREE.Vector3( 28,13,8 ),
+            new THREE.Vector3( 25,13,20),
+
+
+            new THREE.Vector3( -50,20,15 ),
+            new THREE.Vector3( -50,20,-15 ),
+            new THREE.Vector3( 0,10,-19 ),
+
+            //INNER CIRCLE PAINTS
+            new THREE.Vector3( -5,8,-3 ),
+            new THREE.Vector3( -25,8, 0 ),
+            new THREE.Vector3( -28,13, -8 ),
+            new THREE.Vector3( -25,13, -20 ),
+
+            new THREE.Vector3( 60,20,-15 ),
+        ], true );
+        let points = this.curve.getPoints( 50 );
+        let geometry = new THREE.BufferGeometry().setFromPoints( points );
+        let material = new THREE.LineBasicMaterial( { color : 0xff0000 });
+        let curveObject = new THREE.Line( geometry, material );
+        //curveObject.visible = false;
+        //this.scene.add(curveObject);
+
+        this.up = new THREE.Vector3(0, 1, 0);
+        this.targetRotation = new THREE.Quaternion();
+
+        this.PosIndex = 0;
+    }
+
+    MoveOnCurve(){
+        this.PosIndex += 0.25;
+        if (this.PosIndex > 100000) { this.PosIndex = 0;}
+        var camPos = this.curve.getPoint(this.PosIndex / 1000);
+        var camRot = this.curve.getTangent(this.PosIndex / 1000);
+        this.GetBird().position.x = camPos.x;
+        this.GetBird().position.y = camPos.y;
+        this.GetBird().position.z = camPos.z;
+        this.GetBird().rotation.x = camRot.x;
+        this.GetBird().rotation.y = camRot.y;
+        this.GetBird().rotation.z = camRot.z;
+        this.GetBird().lookAt(this.curve.getPoint((this.PosIndex+1) / 1000));
+    }
+
     KeyboardHandling(delta){
-        //const delta = this.clock.getDelta();
         const moveDistance = 5 * delta;
         const rotateAngle = Math.PI / 2 * delta;
-        // if ( this.keyboard.pressed("S") ) {
-        //     this.getBird().translateZ(-moveDistance);
-        // }
+        this.speedUp = 1;
+        //console.log(delta);
+
+        if (this.keyboard.pressed("1")) {
+            this.camera1Active = true;
+            this.camera2Active = false;
+            this.faceCamActive = false;
+        }
+        if (this.keyboard.pressed("2")) {
+            this.camera1Active = false;
+            this.camera2Active = true;
+            this.faceCamActive = false;
+        }
+        if(this.keyboard.pressed("3")){
+            this.camera1Active = false;
+            this.camera2Active = false;
+            this.faceCamActive = true;
+        }
+
+        if (this.keyboard.pressed("shift")){
+            this.speedUp += 2;
+            this.fallSpeed = 0;
+        }else{
+            if(!this.GetBird().position.y <= 4){
+                this.speedUp = 1;
+            }else{
+                this.speedUp = 0;
+            }
+        }
         if ( this.keyboard.pressed("W") ) {
-            this.GetBird().translateZ(moveDistance);
+            this.GetBird().translateZ(moveDistance * speedUp);
             if(this.sideAngle > 0){
                 this.GetBird().translateX(this.sideAngle/2000);
             }
@@ -277,50 +369,22 @@ class MainWorld{
                 this.GetBird().translateX(this.sideAngle/2000);
             }
         }
-        // if ( this.keyboard.pressed("D") ) {
-        //     if(this.GetBird().rotation.z <= Math.PI / 3){ // PRI OTOCENI SA NEOTOCI SSO
-        //         //console.log(Math.abs(this.getBird().rotation.z - this.birdAxis.z));
-        //         this.GetBird().rotateZ(rotateAngle);
-        //         //this.getBird().rotateOnAxis(new THREE.Vector3(0, 0, 2), rotateAngle);
-        //         this.sideAngle -=1;
-        //         console.log('Side angle is ' + this.sideAngle);
-        //
-        //     }
-        //    // this.getBird().position.x -= moveDistance/8;
-        //
-        // }
-        // if ( this.keyboard.pressed("A") ) {
-        //     if(this.GetBird().rotation.z >= -(Math.PI / 3)){ // PRI OTOCENI SA NEOTOCI SSO
-        //         console.log('Side angle is ' + this.sideAngle);
-        //         this.GetBird().rotateZ(-rotateAngle);
-        //         this.sideAngle +=1;
-        //         //this.getBird().rotateOnAxis(new THREE.Vector3(0, 0, 2), -rotateAngle);
-        //     }
-        //     //this.getBird().translateX(moveDistance);
-        // }
-        if(this.keyboard.pressed("space")){
-            console.log(this.GetBird().x);
-            if(this.GetBird().rotation._x >= -0.25) {
-                this.GetBird().rotateOnAxis(new THREE.Vector3(2, 0, 0), -rotateAngle);
-            }
-        }
-        if(this.keyboard.pressed("shift"))
-            if(this.GetBird().rotation._x <= 0.25) {
-                this.GetBird().rotateOnAxis(new THREE.Vector3(2, 0, 0), rotateAngle);
-            }
-        //const rotation_matrix = new THREE.Matrix4().identity();
         if ( this.keyboard.pressed("A") ){
              this.GetBird().rotateOnAxis( new THREE.Vector3(0,2,0),rotateAngle/4);
         }
         if ( this.keyboard.pressed("D") ){
              this.GetBird().rotateOnAxis( new THREE.Vector3(0,2,0),-rotateAngle/4);
         }
-
     }
 }
 
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-    _APP = new MainWorld();
+    Ammo().then((lib) =>{
+        Ammo = lib;
+        _APP = new MainWorld();
+        _APP.RequestAnimationFrame();
+    })
+
 });
